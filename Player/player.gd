f9e93d4e2fc1729523f87da43d45861b4898789d
arch1tect0r <dmitri__13@mail.ru>
2018-11-04 22:55:32 +0300

    Player can walk and jump

--- Player/player.gd
+++ Player/player.gd
@@ -42,16 +42,16 @@ func _physics_process(delta):
 
 	# Horizontal Movement
 	var target_speed = 0
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		target_speed += -1
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		target_speed +=  1
 
 	target_speed *= WALK_SPEED
 	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
 
 	# Jumping
-	if on_floor and Input.is_action_just_pressed("jump"):
+	if on_floor and Input.is_action_just_pressed("ui_up"):
 		linear_vel.y = -JUMP_SPEED
 		#$sound_jump.play()
 
